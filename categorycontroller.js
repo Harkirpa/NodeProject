bf98ec89222c9e803c20925fa4636c076f9af4d0
Harkirpa <kirpa704@gmail.com>
2023-10-22 14:05:35 +0530

    project

--- categorycontroller.js
+++ categorycontroller.js
@@ -1,23 +1,30 @@
-const{bollywood,hollywood,fitness,technology,food}=require('./dummydata')
+const data=require("./dummydata")
 
-const bollywoodController=(req,res)=>{
-    // const id=req.params.id
-    //   const data=bollywood.find((item)=>item.id==id)
-    //   console.log(data)
-    return res.send(bollywood)
+const home=(req,res)=>{
+   return res.send(data)
 }
+module.exports={home}
 
-const hollywoodController=(req,res)=>{
-    return res.send(hollywood)
-}
+// const{bollywood,hollywood,fitness,technology,food}=require('./dummydata')
 
-const fitnessController=(req,res)=>{
-    return res.send(fitness)
-}
-const technologyController=(req,res)=>{
-    return res.send(technology)
-}
-const foodController=(req,res)=>{
-    return res.send(food)
-}
-module.exports={bollywoodController,hollywoodController,fitnessController,technologyController,foodController}
+// const bollywoodController=(req,res)=>{
+//     const id=req.params.id
+//       const data=bollywood.find((item)=>item.id==id)
+//       console.log(data)
+//     return res.send(bollywood)
+// }
+
+// const hollywoodController=(req,res)=>{
+//     return res.send(hollywood)
+// }
+
+// const fitnessController=(req,res)=>{
+//     return res.send(fitness)
+// }
+// const technologyController=(req,res)=>{
+//     return res.send(technology)
+// }
+// const foodController=(req,res)=>{
+//     return res.send(food)
+// }
+// module.exports={bollywoodController,hollywoodController,fitnessController,technologyController,foodController}
--- categoryrouter.js
+++ categoryrouter.js
@@ -1,11 +1,11 @@
-const {bollywoodController,hollywoodController,fitnessController,technologyController,foodController} =require('.//categorycontroller')
+const {home,bollywoodController,hollywoodController,fitnessController,technologyController,foodController} =require('.//categorycontroller')
 
 const categoryRouter=require("express").Router();
 
-categoryRouter.get("/bollywood",bollywoodController)
-categoryRouter.get("/hollywood",hollywoodController)
-categoryRouter.get("/fitness",fitnessController)
-categoryRouter.get("/technology",technologyController)
-categoryRouter.get("/food",foodController)
-
+// categoryRouter.get("/bollywood",bollywoodController)
+// categoryRouter.get("/hollywood",hollywoodController)
+// categoryRouter.get("/fitness",fitnessController)
+// categoryRouter.get("/technology",technologyController)
+// categoryRouter.get("/food",foodController)
+categoryRouter.get("/",home)
 module.exports=categoryRouter;
