68aec3be5f30892c6584f0560708c04c766d1234
Harkirpa <kirpa704@gmail.com>
2023-10-22 17:47:26 +0530

    project

--- categoryrouter.js
+++ categoryrouter.js
@@ -1,11 +1,11 @@
-const {home,bollywoodController,hollywoodController,fitnessController,technologyController,foodController} =require('.//categorycontroller')
+const {homeController,bollywoodController,hollywoodController,fitnessController,technologyController,foodController} =require('.//categorycontroller')
 
 const categoryRouter=require("express").Router();
 
-// categoryRouter.get("/bollywood",bollywoodController)
-// categoryRouter.get("/hollywood",hollywoodController)
-// categoryRouter.get("/fitness",fitnessController)
-// categoryRouter.get("/technology",technologyController)
-// categoryRouter.get("/food",foodController)
-categoryRouter.get("/",home)
+categoryRouter.get("/bollywood",bollywoodController)
+categoryRouter.get("/hollywood",hollywoodController)
+categoryRouter.get("/fitness",fitnessController)
+categoryRouter.get("/technology",technologyController)
+categoryRouter.get("/food",foodController)
+categoryRouter.get("/home",homeController)
 module.exports=categoryRouter;
